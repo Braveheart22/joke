c81eaf6620942feeb5a6c92879a0076713c240e2
John Strack <Braveheart22@GMail.com>
2018-03-20 14:18:05 -0400

    Use notification service

--- app/src/main/java/com/johnstrack/joke/DelayedMessageService.java
+++ app/src/main/java/com/johnstrack/joke/DelayedMessageService.java
@@ -1,7 +1,10 @@
 package com.johnstrack.joke;
 
 import android.app.IntentService;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 
@@ -13,6 +16,7 @@ import android.util.Log;
 public class DelayedMessageService extends IntentService {
 
     public static final String EXTRA_MESSAGE = "message";
+    public static final int NOTIFICATION_ID = 5453;
     private static final String TAG = "DelayedMessageService";
 
     public DelayedMessageService() {
@@ -34,5 +38,27 @@ public class DelayedMessageService extends IntentService {
 
     private void showText (final String text) {
         Log.v(TAG, "showText: The message is: " + text);
+
+        //Create a notification builder
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this)
+                .setSmallIcon(android.R.drawable.sym_def_app_icon)
+                .setContentTitle(getString(R.string.question))
+                .setContentText(text)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setVibrate(new long[] {0, 1000})
+                .setAutoCancel(true);
+
+        //Create an action
+        Intent actionIntent = new Intent(this, MainActivity.class);
+        PendingIntent actionPendingIntent = PendingIntent.getActivity(
+                this,
+                0,
+                actionIntent,
+                PendingIntent.FLAG_UPDATE_CURRENT);
+        builder.setContentIntent(actionPendingIntent);
+
+        //Issue notification
+        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        notificationManager.notify(NOTIFICATION_ID, builder.build());
     }
 }
